ccb5138a7cdb615c5dec3b183f1b66cb1c94b003
America Rivas <amerivasmur@gmail.com>
2021-02-14 18:21:10 -0400

    Creando un arreglo de objetos

--- EscuelaEtapa1/Program.cs
+++ EscuelaEtapa1/Program.cs
@@ -20,7 +20,7 @@ namespace EscuelaEtapa1
             //En este caso, cada vez que se quiera llamar a una cadena, se imprime el metodo sobre-escrito
             Console.WriteLine(escuela);
 
-            var curso1 = new Curso() { 
+            /*var curso1 = new Curso() { 
                 Nombre = "101"
             };
 
@@ -32,17 +32,54 @@ namespace EscuelaEtapa1
             var curso3 = new Curso()
             {
                 Nombre = "301"
-            };
+            };*/
 
             //Imprimir una linea
-            System.Console.WriteLine("----------------");
+            //System.Console.WriteLine("----------------");
             //Imprimir la informacion de los cursos
-            Console.WriteLine(curso1.Nombre + "," + curso1.UniqueId);
+            //Console.WriteLine(curso1.Nombre + "," + curso1.UniqueId);
             //Otro tipo de impresion
-            Console.WriteLine($"{curso2.Nombre},{curso2.UniqueId}");
-            Console.WriteLine(curso3);
+            //Console.WriteLine($"{curso2.Nombre},{curso2.UniqueId}");
+            //Console.WriteLine(curso3);
+
+            //COMO HACER PARA MANEJAR MULTIPLES OBJETOS EN LA ESCUELA?
+            //Pues, creamos un arreglo de objetos
+            //3 formas de crearlo:
+            var arregloCursos = new Curso[3];
+
+            arregloCursos[0] = new Curso()
+            {
+                Nombre = "101"
+            };
+
+            var curso2 = new Curso()
+            {
+                Nombre = "201"
+            };
+            arregloCursos[1] = curso2;
+
+            arregloCursos[2] = new Curso
+            {
+                Nombre = "301"
+            };
+
+            System.Console.WriteLine("----------------");
+               
+            //Forma de imprimir por la generacion de un metodo
+            ImprimirCursos(arregloCursos);
+
 
             Console.ReadKey();
+        }
+
+        private static void ImprimirCursos(Curso[] arregloCursos)
+        {
+            int contador = 0;
+            while (contador < arregloCursos.Length)
+            {
+                Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, Id{arregloCursos[contador].UniqueId}");
+                contador++;
+            }
         }
     }
 }
