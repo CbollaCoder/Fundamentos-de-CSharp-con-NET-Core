ffb6adbb611688c7a4f6d4633e90f31de190d5e9
America Rivas <amerivasmur@gmail.com>
2021-02-18 23:20:03 -0400

    Curso Csharp con .Net Core - Uso de Regiones

--- EscuelaEtapa1/App/EscuelaEngine.cs
+++ EscuelaEtapa1/App/EscuelaEngine.cs
@@ -27,6 +27,44 @@ namespace EscuelaEtapa1.App
 
         }
 
+        //Metodo que devuelve todos los objetos "escuela" que contiene
+        public List<ObjetoEscuelaBase> GetObjetosEscuela()
+        {
+            var listObj = new List<ObjetoEscuelaBase>();
+            listObj.Add(Escuela);
+            listObj.AddRange(Escuela.Cursos);
+            foreach (var curso in Escuela.Cursos)
+            {
+                listObj.AddRange(curso.Asignaturas);
+                listObj.AddRange(curso.Alumnos);
+
+                foreach (var alumno in curso.Alumnos)
+                {
+                    listObj.AddRange(alumno.Evaluaciones);
+                }
+            }
+            return listObj;
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar(int cantidad)
+        {
+            string[] nombre1 = { "Alba", "Felipe", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Morty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+
+            //El OrderBy Funciona como DELEGADO
+            return listaAlumnos.OrderBy((al) => al.UniqueId).Take(cantidad).ToList();
+
+        }
+
+
+        #region Métodos de Carga
+
         private void CargarEvaluaciones()
         {
             
@@ -74,22 +112,6 @@ namespace EscuelaEtapa1.App
             }
         }
 
-        private List<Alumno> GenerarAlumnosAlAzar(int cantidad)
-        {
-            string[] nombre1 = { "Alba","Felipe","Eusebio","Farid","Donald","Alvaro","Nicolás" };
-            string[] apellido1 = { "Ruiz","Sarmiento","Uribe","Maduro","Trump","Toledo","Herrera"};
-            string[] nombre2 = { "Freddy","Anabel","Rick","Morty","Silvana","Diomedes","Nicomedes","Teodoro"};
-
-            var listaAlumnos = from n1 in nombre1
-                               from n2 in nombre2
-                               from a1 in apellido1
-                               select new Alumno { Nombre= $"{n1} {n2} {a1}" };
-
-            //El OrderBy Funciona como DELEGADO
-            return listaAlumnos.OrderBy((al) => al.UniqueId).Take(cantidad).ToList();
-            
-        }
-
         private void CargarCursos()
         {
             Escuela.Cursos = new List<Curso>() {
@@ -112,24 +134,7 @@ namespace EscuelaEtapa1.App
             }
         }
 
-        //Metodo que devuelve todos los objetos "escuela" que contiene
-        public List<ObjetoEscuelaBase> GetObjetosEscuela()
-        {
-            var listObj = new List<ObjetoEscuelaBase>();
-            listObj.Add(Escuela);
-            listObj.AddRange(Escuela.Cursos);
-            foreach (var curso in Escuela.Cursos)
-            {
-                listObj.AddRange(curso.Asignaturas);
-                listObj.AddRange(curso.Alumnos);
-
-                foreach (var alumno in curso.Alumnos)
-                {
-                    listObj.AddRange(alumno.Evaluaciones);
-                }
-            }
-            return listObj;
-        }
+        #endregion
 
     }
 }
